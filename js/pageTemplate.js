28a85d033f049df0f2bcf97033da12f8ed611c03
dirk-woehr <51853046+dirk-woehr@users.noreply.github.com>
2026-03-01 23:57:38 +0100

    Fix menu on mobil horz

--- js/pageTemplate.js
+++ js/pageTemplate.js
@@ -1,32 +1,29 @@
 class HeaderSection extends HTMLElement {
-  
-connectedCallback(){
-  this.innerHTML = `<img src="images/logo.png" alt="Band-Logo">`
+  connectedCallback() {
+    this.innerHTML = `<img src="images/logo.png" alt="Band-Logo">`;
   }
 }
-customElements.define('header-section', HeaderSection);
+customElements.define("header-section", HeaderSection);
 
-class MenuSection extends HTMLElement {  
-  connectedCallback(){
+class MenuSection extends HTMLElement {
+  connectedCallback() {
     const pages = [
-      { id: 'home', url: "index.html", name: 'Home'},
-      { id: 'gigs', url: "gigs.html", name: 'Gigs'},
-      { id: 'news', url: "news.html", name: 'News'},
-      { id: 'releases', url: "releases.html", name: 'Releases'},
-      { id: 'band', url: "band.html", name: 'Band'},
-      { id: 'press', url: "press.html", name: 'Press'},
-      { id: 'gallery', url: "gallery.html", name: 'Gallery'},
-      { id: 'links', url: "links.html", name: 'Links'},
+      { id: "home", url: "index.html", name: "Home" },
+      { id: "gigs", url: "gigs.html", name: "Gigs" },
+      { id: "news", url: "news.html", name: "News" },
+      { id: "releases", url: "releases.html", name: "Releases" },
+      { id: "band", url: "band.html", name: "Band" },
+      { id: "press", url: "press.html", name: "Press" },
+      { id: "gallery", url: "gallery.html", name: "Gallery" },
+      { id: "links", url: "links.html", name: "Links" },
     ];
-
 
     const pageStrings = [];
 
-    pages.forEach(page => {
-        pageStrings.push(`<li class="menuItem">
+    pages.forEach((page) => {
+      pageStrings.push(`<li class="menuItem">
           <a class="pageLink ${page.id}" href="${page.url}">${page.name}</a>
-        </li>`
-      );
+        </li>`);
     });
 
     this.innerHTML = `
@@ -35,10 +32,12 @@ class MenuSection extends HTMLElement {
           <span></span>
           <span></span>
         </button>
-        <ul id="menuList">
-        ${pageStrings.join("")}
-        </ul>
-        <a href="#header" id="scrollToMenu">⤒</a>`
+        <div id="menuListContainer">
+          <ul id="menuList">
+          ${pageStrings.join("")}
+          </ul>
+        </div>  
+        <a href="#header" id="scrollToMenu">⤒</a>`;
   }
 }
-customElements.define('menu-section', MenuSection);
+customElements.define("menu-section", MenuSection);
