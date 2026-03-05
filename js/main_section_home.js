f0c75100fb46f8e64fe5744eacd9e53f414cdbb2
dirk-woehr <51853046+dirk-woehr@users.noreply.github.com>
2026-03-05 18:38:59 +0100

    Add welcome text & image

--- js/main_section_home.js
+++ js/main_section_home.js
@@ -4,6 +4,16 @@ import { renderGigs } from "./render_gigs.js";
 class MainSection extends HTMLElement {
   connectedCallback() {
     this.innerHTML = "";
+/* 
+*/
+    const homeSection = document.createElement("section");
+    homeSection.id = "homeSection";
+    homeSection.innerHTML = `<h1>Welcome to Accion Mutante's Official Website</h1>
+    <img src="../images/home.jpg" alt="Accion Mutante Band Image">
+    <h2>Discover our latest news, upcoming gigs, and exclusive content. Stay connected with us for all the latest updates and behind-the-scenes insights.</h2>
+    `;
+
+    this.appendChild(homeSection);
 
     renderGigs(this);
 
