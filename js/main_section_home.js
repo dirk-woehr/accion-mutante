5efa59dce7c441fd425b76f510865863bafc63c8
dirk-woehr <51853046+dirk-woehr@users.noreply.github.com>
2026-03-05 19:44:19 +0100

    Fix image path

--- js/main_section_home.js
+++ js/main_section_home.js
@@ -8,7 +8,7 @@ class MainSection extends HTMLElement {
     const homeSection = document.createElement("section");
     homeSection.id = "homeSection";
     homeSection.innerHTML = `<h1>Welcome to Accion Mutante's Official Website</h1>
-    <img src="../images/home.jpg" alt="Accion Mutante Band Image">
+    <img src="./images/home.jpg" alt="Accion Mutante Band Image">
     <h2>Discover our latest news, upcoming gigs, and exclusive content. Stay connected with us for all the latest updates and behind-the-scenes insights.</h2>
     `;
 
