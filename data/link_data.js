91138ecbf7ca39328125b0e71bc7fe7ab47fec35
dirk-woehr <51853046+dirk-woehr@users.noreply.github.com>
2026-03-07 21:47:46 +0100

    Update images

--- data/link_data.js
+++ data/link_data.js
@@ -1,65 +1,65 @@
 const bandLinks = [
-  { 
+  {
     name: "Belching Beet",
     url: "#",
   },
-  { 
+  {
     name: "Cluster Bomb Unit",
     url: "#",
   },
-  { 
+  {
     name: "Nasum",
     url: "#",
   },
-  { 
+  {
     name: "Murder Disco Experience",
     url: "#",
-  }
+  },
 ];
 
 const labelLinks = [
-  { 
+  {
     name: "Crimes Against Humanity",
     url: "#",
-    image: "./images/labels/crimeslogo.gif"
+    image: "./images/labels/crimeslogo.gif",
   },
-  { 
+  {
     name: "Cudgel",
-    url: "#",
-    image: "./images/labels/cudgel.jpg"
+    url: "https://www.cudgel.de",
+    image: "./images/labels/cudgel-logo-neu-1-1400x419.png",
   },
-  { 
+  {
     name: "Relapse",
     url: "#",
-    image: "./images/labels/relapse.gif"
+    image: "./images/labels/relapse.gif",
   },
-  { 
+  {
     name: "Shizuka Kollektiv",
     url: "#",
-    image: "./images/labels/sk-logo-klein.jpg"
-  }
-]
+    image: "./images/labels/sk-logo-klein.png",
+  },
+];
 
 const miscLinks = [
-  { 
+  {
     name: "Campaign for Musical Destruction",
     url: "#",
-    image: "./images/misc/campaign.gif"
+    image: "./images/misc/campaign.gif",
   },
-  { 
+  {
     name: "Crustpunk",
     url: "#",
   },
-  { 
+  {
     name: "Jugendhaus West / Stuttgart",
     url: "#",
-    image: "./images/misc/juha-west.png"
+    image: "./images/misc/juha-west.png",
   },
-  { 
+  {
     name: "Schwarzer Keiler",
     url: "#",
-  }
-]
+  },
+];
 
 export const links = {
   bands: [
@@ -76,6 +76,6 @@ export const links = {
     ...bandLinks,
     ...bandLinks,
   ],
-  labels: [ ...labelLinks, ...labelLinks],
-  misc: [...miscLinks, ...miscLinks]
+  labels: [...labelLinks, ...labelLinks],
+  misc: [...miscLinks, ...miscLinks],
 };
